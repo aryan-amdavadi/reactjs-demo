e24fa5d571b5b152a1e356ab83308805a06d9356
aryan-amdavadi <aryanb4256@gmail.com>
2025-05-30 17:57:37 +0530

    imported Router In React

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
